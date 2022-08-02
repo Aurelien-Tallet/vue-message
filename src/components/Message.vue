e707aed3b1cc0f9833c941811f3eb299fd3cdbad
Aurelien-Tallet <aurelien.jarte@gmail.com>
2022-08-02 10:20:43 +0200

    first commit

---
+++ src/components/Message.vue
@@ -0,0 +1,31 @@
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Message",
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+  setup({ msg }) {
+    const msgRef = ref("");
+    if (msg) {
+      localStorage.setItem("msg", msg);
+    }
+    msgRef.value = localStorage.getItem("msg");
+    return {
+      msgRef,
+    }
+  },
+});
+</script>
+<template>
+  <h1>{{ msgRef }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
